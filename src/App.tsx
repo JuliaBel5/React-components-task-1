130032a1aa5707b01e150e6b9c6a09f99d8075ad
JuliaBel5 <juliabel5@gmail.com>
2023-11-13 02:03:23 +0300

    refactor: code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import './App.css'
 import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { CatList } from './components/CatList/CatList'
 import { CatBreed } from './services/CatService'
 import { SearchDataContext, SearchResultsContext } from './store/SearchContext'
 
 export const App: React.FC<React.ComponentPropsWithoutRef<'div'>> = () => {
+  const [searchParams] = useSearchParams()
+
   const [searchTerm, setSearchTerm] = useState<string>(
-    localStorage.getItem('searchTerm') ?? '',
+    localStorage.getItem('searchTerm') ??
+      searchParams.get('urlSearchTerm') ??
+      '',
   )
   const [searchResults, setSearchResults] = useState<CatBreed[]>([])
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentPage, setCurrentPage] = useState<number>(
+    Number(searchParams.get('page')) || 1,
+  )
   const [totalPages, setTotalPages] = useState<number>(2)
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [limit, setLimit] = useState<number>(6)
+  const [limit, setLimit] = useState<number>(
+    Number(searchParams.get('limit')) || 6,
+  )
   const [cat, setCat] = useState<CatBreed | null>(null)
   return (
     <div className="App">
