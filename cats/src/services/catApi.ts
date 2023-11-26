f814a9e1f4cb9621f60bda5ac6ffb935b888a1f1
JuliaBel5 <juliabel5@gmail.com>
2023-11-27 01:10:08 +0300

    fix: fix ts bug

--- cats/src/services/catApi.ts
+++ cats/src/services/catApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { HYDRATE } from 'next-redux-wrapper'
+//@ts-ignore
 import fetchFn from 'whatwg-fetch'
 
 const baseUrl = `https://2ff5030c446d8ca4.mokky.dev/breeds`
